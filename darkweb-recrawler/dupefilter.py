fa4578d2ecacef45682fac02e02f439751950637
Isuranga Perera <isurangamperera@gmail.com>
2021-08-21 20:35:19 +0300

    Fix request fingerprinting

--- darkweb-recrawler/dupefilter.py
+++ darkweb-recrawler/dupefilter.py
@@ -1,7 +1,9 @@
+import hashlib
+
 from scrapy_redis.dupefilter import RFPDupeFilter
-from scrapy_splash.dupefilter import splash_request_fingerprint
 
 
 class CustomRFPDupeFilter(RFPDupeFilter):
     def request_fingerprint(self, request):
-        return splash_request_fingerprint(request)
+        url = request.url
+        return hashlib.sha1(url.encode("utf-8")).hexdigest()
