f117ad02a462b6026ccadfe5e8815d7b58373447
Isuranga Perera <isurangamperera@gmail.com>
2021-08-22 11:59:13 +0300

    Fix js, css content persistence

--- darkweb-recrawler/pipelines.py
+++ darkweb-recrawler/pipelines.py
@@ -61,19 +61,21 @@ class TorPipeline(object):
             with open("{root}/{path}".format(root=root_dir, path=screenshot_path), mode="wb") as f:
                 f.write(screenshot)
         if len(js) > 0:
-            for i in range(len(js)):
-                js_path = "{js}/{index}.js".format(js=js_dir, index=i)
+            for js_url, content in js.items():
+                js_url_hash = sha256(js_url.encode("utf-8")).hexdigest()
+                js_path = "{js}/{index}.js".format(js=js_dir, index=js_url_hash)
                 js_paths.append(js_path)
                 with open("{root}/{js}".format(root=root_dir, js=js_path), encoding="utf-8", mode="w",
                           errors='ignore') as f:
-                    f.write(js[i])
+                    f.write(content)
         if len(css) > 0:
-            for i in range(len(css)):
-                css_path = "{css}/{index}.css".format(css=css_dir, index=i)
+            for css_url, content in css.items():
+                css_url_hash = sha256(css_url.encode("utf-8")).hexdigest()
+                css_path = "{css}/{index}.css".format(css=css_dir, index=css_url_hash)
                 css_paths.append(css_path)
                 with open("{root}/{css}".format(root=root_dir, css=css_path), encoding="utf-8", mode="w",
                           errors='ignore') as f:
-                    f.write(css[i])
+                    f.write(content)
 
         item["page"] = {
             "raw_md5": item["raw_md5"],
