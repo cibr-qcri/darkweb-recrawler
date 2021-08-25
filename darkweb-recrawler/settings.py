a9c4a71e880d4c9ce476f950bbc84eed7ab9ee94
Isuranga Perera <isurangamperera@gmail.com>
2021-08-25 10:00:11 +0300

    Temporary js crawler

--- darkweb-recrawler/settings.py
+++ darkweb-recrawler/settings.py
@@ -88,15 +88,15 @@ NEWSPIDER_MODULE = 'darkweb-recrawler.spiders'
 # ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 2
+#CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 # DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 2
-CONCURRENT_REQUESTS_PER_IP = 2
+#CONCURRENT_REQUESTS_PER_DOMAIN = 2
+#CONCURRENT_REQUESTS_PER_IP = 2
 
 # Disable cookies (enabled by default)
 # COOKIES_ENABLED = False
